c30f886cbc12ebb8f177aa014fb6cae58eb17e4a
Clifford Darko Opoku-Sarkodie <137389025+cliff-de-tech@users.noreply.github.com>
2026-02-14 14:40:17 +0000

    Task 0: Initial setup and project configuration for Django blog

---
+++ django_blog/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    return render(request, 'blog/base.html')
+
+
+def posts(request):
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, 'blog/posts.html', {'posts': posts})
