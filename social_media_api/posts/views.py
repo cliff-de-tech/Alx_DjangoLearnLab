5df57e1ae9ef62dd5f83038a1f798f626577eee8
Clifford Darko Opoku-Sarkodie <137389025+cliff-de-tech@users.noreply.github.com>
2026-02-19 20:56:56 +0000

    Fix: rename followed_users to following_users in FeedView.get_queryset

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -39,5 +39,5 @@ class FeedView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        followed_users = self.request.user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
